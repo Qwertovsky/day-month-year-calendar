b23321c5c5e1df2b720c0ee30294e48774a9e054
Qwertovsky <qwertovsky.gmail.com>
2014-11-10 22:50:25 +0300

    Add options

--- day-month-year-calendar.jquery.js
+++ day-month-year-calendar.jquery.js
@@ -24,9 +24,24 @@ DayMonthYearCalendar = (function() {
 
     function DayMonthYearCalendar(form_field, options) {
       this.form_field = form_field;
-      this.options = options || {};
       this.form_field_jq = $(form_field);
+      this.options = options || {};
+      this.setup_default_options();
       this.setup_html();
+    };
+
+    DayMonthYearCalendar.prototype.setup_default_options = function() {
+      var currentDate = new Date();
+      this.minDate = this.options.minDate || new Date(currentDate.setFullYear(currentDate.getFullYear() - 100));
+      this.maxDate = this.options.maxDate || new Date(currentDate.setFullYear(currentDate.getFullYear() + 100));
+      this.fullMonthArray = this.options.monthNames || [];
+      this.daysClass = this.options.daysClass || 'dmy-cal-days';
+      this.monthsClass = this.options.monthsClass || 'dmy-cal-months';
+      this.yearsClass = this.options.yearsClass || 'dmy-cal-years';
+      this.disabled = this.form_field_jq.prop('disabled');
+      this.daysEmptyText = this.options.daysEmptyText || 'dd';
+      this.monthsEmptyText = this.options.monthsEmptyText || 'mm';
+      this.yearsEmptyText = this.options.yearsEmptyText || 'yyyy';
     };
 
     DayMonthYearCalendar.prototype.setup_html = function() {
@@ -35,35 +50,34 @@ DayMonthYearCalendar = (function() {
         'class': this.form_field.className
       };
       this.container = $("<div />", container_props);
-      this.container.html('<div class="day_select">'
+      this.container.html('<div class="' + this.daysClass + '">'
                           + '    <select id="days"></select>'
                           + '</div>'
-                          + '<div class="month_select">'
+                          + '<div class="' + this.monthsClass + '">'
                           + '  <select id="months"></select>'
                           + '</div>'
-                          + '<div class="year_select">'
+                          + '<div class="' + this.yearsClass + '">'
                           + '  <select id="years"></select>'
                           + '</div>');
       this.years = this.container.find('#years');
       this.months = this.container.find('#months');
       this.days = this.container.find('#days');
 
-      this.years.append($('<option selected value="0">год</option>'));
-      for (i = new Date().getFullYear(); i > 1900; i--) {
+
+      this.years.append($('<option selected value="0">' + this.yearsEmptyText + '</option>'));
+      for (var i = this.maxDate.getFullYear(); i >= this.minDate.getFullYear(); i--) {
         this.years.append($('<option />').val(i).html(i));
       }
-      this.months.append($('<option selected value="0">месяц</option>'));
-      var fullMonthArray = this.options.monthNames || [];
-      for (i = 1; i < 13; i++) {
 
-        this.months.append($('<option />').val(i).html(fullMonthArray[i-1] || i));
+      this.months.append($('<option selected value="0">' + this.monthsEmptyText + '</option>'));
+      for (var i = 1; i < 13; i++) {
+        this.months.append($('<option />').val(i).html(this.fullMonthArray[i-1] || i));
       }
       
       this.form_field_jq.hide();
       this.form_field_jq.after(this.container);
 
       this.load_form_field();
-      this.update_number_of_days();
 
       this.container.find('#years, #months').change(function () {
         _this.update_number_of_days(); 
@@ -72,7 +86,7 @@ DayMonthYearCalendar = (function() {
           _this.update_form_field();
         });
 
-      
+      this.container.find('select').prop('disabled', this.disabled);
     };
 
     DayMonthYearCalendar.prototype.update_number_of_days = function() {
@@ -82,7 +96,7 @@ DayMonthYearCalendar = (function() {
       year = this.years.val();
       days = this.days_in_month(month, year);
 
-      this.days.append($('<option selected value="0">дд</option>'));
+      this.days.append($('<option selected value="0">' + this.daysEmptyText + '</option>'));
       for (i=1; i < days+1 ; i++) {
         this.days.append($('<option />').val(i).html(i));
       }
@@ -102,17 +116,22 @@ DayMonthYearCalendar = (function() {
         this.form_field_jq.val(this.pad_date_field(day,2)
           + '.' + this.pad_date_field(month,2)
           + '.' + this.pad_date_field(year,4));
-        this.form_field_jq.change();
+      } else {
+        this.form_field_jq.val('');
       }
+      this.form_field_jq.change();
     };
 
     DayMonthYearCalendar.prototype.load_form_field = function() {
       var value = this.form_field_jq.val();
       if (value && value.length == 10) {
         var dateFields = value.split('.');
-        this.days.val(parseInt(dateFields[0], 10));
         this.months.val(parseInt(dateFields[1], 10));
         this.years.val(parseInt(dateFields[2], 10));
+        this.update_number_of_days();
+        this.days.val(parseInt(dateFields[0], 10));
+      } else {
+        this.update_number_of_days();
       }
     };
 
@@ -122,6 +141,12 @@ DayMonthYearCalendar = (function() {
 
     DayMonthYearCalendar.prototype.pad_date_field = function(value, size) {
       return ('0000' + value).substr(-size); 
+    };
+
+    DayMonthYearCalendar.prototype.destroy = function() {
+      this.container.remove();
+      this.form_field_jq.removeData('dayMonthYearCalendar');
+      return this.form_field_jq.show();
     };
 
     return DayMonthYearCalendar;

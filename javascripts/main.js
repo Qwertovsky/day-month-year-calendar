0e8717b7593ccaa3723dc4ac1c31fa0475b40028
Qwertovsky <qwertovsky@gmail.com>
2015-01-25 20:58:45 +0300

    edit page

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,32 @@
-console.log('This would be the main JS file.');
+$('.example_input').val($.datepicker.formatDate('dd.mm.yy', new Date()));
+
+$('#example_1 .example_input').dayMonthYearCalendar({
+    container: $('#example_1 .selects_container')
+    , hideInput: false
+});
+$('#example_2 .example_input').dayMonthYearCalendar({
+    container: $('#example_2 .selects_container')
+    , hideInput: false
+    , monthNames: $.datepicker._defaults.monthNames
+});
+$('#example_3 .example_input').dayMonthYearCalendar({
+    container: $('#example_3 .selects_container')
+    , hideInput: false
+    , monthNames: $.datepicker._defaults.monthNames
+    , minDate: new Date(2010, 3, 15)
+    , maxDate: new Date()
+});
+$('#dateinput_4').val($.datepicker.formatDate('MM d, yy', new Date()));
+$('#example_4 .example_input').dayMonthYearCalendar({
+    container: $('#example_4 .selects_container')
+    , hideInput: false
+    , monthNames: $.datepicker._defaults.monthNames
+    , minDate: new Date(2010, 3, 15)
+    , maxDate: new Date()
+    , dateFormatFunction: function(date) {
+    	return $.datepicker.formatDate('MM d, yy',date);
+    }
+    , dateParseFunction: function(dateString){
+    	return moment(dateString, 'MMM D, YYYY').toDate();
+    }
+});
